4d15e95c9ec8c64fb2416df704b74a5047bac2ef
eldardiusheev <khandusheev@gmail.com>
2023-09-29 14:49:51 +0300

    updating pip installments

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Job_Ad_Application\mysite\env
+set VIRTUAL_ENV=C:\Users\Днс\Desktop\Job-Site-Project\env
 
 if not defined PROMPT set PROMPT=$P$G
 
